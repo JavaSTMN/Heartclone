ccd68da75958e500cdc9329612b03b09b641e9e6
Adrien Bonnamour <adrien@bonnamour.me>
2019-11-13 14:36:13 +0100

    Implemented Target & Attacker interfaces

--- src/Target.java
+++ src/Target.java
@@ -7,5 +7,16 @@
  *
  */
 public interface Target {
-
+	/**
+	 * Substract healthpoints by the specified number.
+	 * @throws IllegalArgumentException if nb < 0
+	 * @param nb int the number of damage to be taken
+	 */
+	public void receiveDamage (int nb) throws IllegalArgumentException;
+	
+	/**
+	 * Checks if the target has more than 0 healthpoints
+	 * @return boolean true if healthpoints > 0 | false otherwise
+	 */
+	public boolean isAlive();
 }
