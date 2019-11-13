13b4d68add4cad1a9c82ae4af00c2c60861073ad
Bastien CHAUMARD <bastien.chaumard@live.fr>
2019-11-13 16:11:23 +0100

    hero and game manager start

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,3 +1,12 @@
+import java.lang.reflect.Array;
+import java.sql.Date;
+import java.sql.Time;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import javax.swing.SwingWorker;
+
 /**
  * 
  */
@@ -8,11 +17,65 @@
  */
 public class GameManager {
 
-	/**
-	 * 
-	 */
-	public GameManager() {
-		// TODO Auto-generated constructor stub
+	private Hero opponents[];
+	private int turnCount;
+	private Date turnStartDate;
+	private long turnMaxSeconds;
+	
+	
+	public GameManager()
+	{
+		opponents = new Hero[2];
+		turnCount = 0;		
+		turnMaxSeconds = 30;
+		turnStartDate = Date.valueOf(LocalDate.now());
+	}
+	
+	public void startGame()
+	{
+		
+	}
+	
+	public void finishGame()
+	{
+		
+	}
+	
+	public void startTurn(Hero hero)
+	{
+		turnStartDate = Date.valueOf(LocalDate.now());
+		inTurn(hero);
+	}
+	
+	public void inTurn(Hero hero)
+	{
+		SwingWorker sw = new SwingWorker()
+		{
+			protected Object doInBackground() throws Exception
+			{
+				do {} while (turnTimeLeft() > 0);
+				return null;
+			}
+			
+			public void done()
+			{
+				finishTurn(hero);
+			}
+		};
+		
+		
+		sw.execute();
+	}
+	
+	public void finishTurn(Hero hero)
+	{
+		
+	}
+	
+	private long turnTimeLeft()
+	{			
+		long diff = (turnStartDate.getTime()/1000) + turnMaxSeconds - (Date.valueOf(LocalDate.now()).getTime()/1000);
+		return diff;
 	}
 
 }
--- src/Hero.java
+++ src/Hero.java
@@ -1,3 +1,5 @@
+import java.awt.Image;
+
 /**
  * 
  */
@@ -7,12 +9,73 @@
  *
  */
 public class Hero implements Attacker, Target {
-
-	/**
-	 * 
-	 */
+	
+	private int cristals;
+	private int lifePoints;
+	private Deck deck;
+	private CardContainer gameboard;
+	private Image image;
+	
+	
+	
 	public Hero() {
-		// TODO Auto-generated constructor stub
+		cristals = 1;
+		deck = new Deck();
+		gameboard = new CardContainer();
+		lifePoints = 30;
+//		image
+	}
+	
+	public void play(Card playableCard) {
+		
+	}
+	
+	public void draw() {
+		
+	}
+	
+	public void useSpell() {
+		
+	}
+	
+	public void useCristal(int nbCristalsUsed) {
+		cristals -= nbCristalsUsed;
+	}
+
+	@Override
+	public void receiveDamage(int nb) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isAlive() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void dealDamage(Target target) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void disable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void enable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean getState() {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
