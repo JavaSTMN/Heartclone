d352ac54b1a65fc33e0dcb33c4378ee3f7f4c8bf
Alexandre Tomasia <alexandre.tomasia@gmail.com>
2019-11-13 17:01:26 +0100

    creation of the minnionCard class

--- src/MinionCard.java
+++ src/MinionCard.java
@@ -4,15 +4,135 @@
 
 /**
  * @author adrien
+ * @author MadKid
  *
  */
-public class MinionCard extends Card {
+public class MinionCard extends Card implements Attacker, Target {
+
+	private int healthPoints;
+	private int maxHealtPoints;
+	private int damagePoints;
+	private boolean active;
 
 	/**
+	 * Constructor
 	 * 
+	 * @param healthPoints
+	 * @param damagePoints
+	 * @param active
 	 */
-	public MinionCard() {
-		// TODO Auto-generated constructor stub
+	public MinionCard(int maxHealthPoints, int damagePoints, boolean active) {
+		this.maxHealtPoints = this.healthPoints = maxHealthPoints;
+		this.damagePoints = damagePoints;
+		this.active = active;
+	}
+
+	public int getHealthPoints() {
+		return this.healthPoints;
+	}
+
+	public void setHealthPoints(int value) {
+		this.healthPoints = value;
+	}
+
+	public int getDamagePoints() {
+		return this.damagePoints;
+	}
+
+	public void setDamagePoints(int value) {
+		this.damagePoints = value;
+	}
+
+	/* METHODS REGION */
+
+	// Attacker Interface //
+
+	/**
+	 * Allow the minion to attack a target
+	 * 
+	 * @param Target target: the card or Hero which will receive the damage
+	 */
+	@Override
+	public void dealDamage(Target target) {
+		target.receiveDamage(this.damagePoints);
+	}
+
+	/**
+	 * Disable the card. it's state becomes inactive
+	 */
+	@Override
+	public void disable() {
+		this.active = false;
+	}
+
+	/**
+	 * Enable the card. it's state becomes active
+	 */
+	@Override
+	public void enable() {
+		this.active = true;
+	}
+
+	/**
+	 * Return the state of the card
+	 * 
+	 * @return true if the card is active, false if not
+	 */
+	@Override
+	public boolean getState() {
+		return this.active;
+	}
+
+	// Target Interface //
+
+	/**
+	 * Allows the minion to receive damage.
+	 * 
+	 * @throws throw IllegalArgumentException if nb < 0
+	 * @param int nb: amount of damage received
+	 */
+	@Override
+	public void receiveDamage(int nb) throws IllegalArgumentException {
+
+		if (nb < 0)
+			throw new IllegalArgumentException("Amount of damage is less than 0");
+
+		this.healthPoints -= nb;
+
+		if (!isAlive()) {
+			// TODO: destroy this object
+			System.out.println("La carte a été détruite");
+		}
+	}
+
+	/**
+	 * Allows the minion to receive health points
+	 * 
+	 * @throws throw IllegalArgumentException if amount < 0
+	 * @param amount int: the amount of health to receive
+	 */
+	@Override
+	public void receiveHealthPoints(int amount) throws IllegalArgumentException {
+
+		if (amount < 0)
+			throw new IllegalArgumentException("Amount of heal is less than 0");
+
+		if (this.healthPoints + amount > this.maxHealtPoints)
+			this.healthPoints = this.maxHealtPoints;
+		else
+			this.healthPoints += amount;
+	}
+
+	/**
+	 * Inform if the minion is alive or not
+	 * 
+	 * @return true if the minion is alive, false if not
+	 */
+	public boolean isAlive() {
+		if (this.healthPoints < 0)
+			return false;
+
+		return true;
 	}
 
 }
--- src/Target.java
+++ src/Target.java
@@ -15,6 +15,13 @@ public interface Target {
 	public void receiveDamage (int nb) throws IllegalArgumentException;
 	
 	/**
+	 * Add the specified amount to the healtpoints
+	 * @throws IllegalArgumentException if amount < 0
+	 * @param amount int: the amount of health points to add
+	 */
+	public void receiveHealthPoints(int amount) throws IllegalArgumentException;
+	
+	/**
 	 * Checks if the target has more than 0 healthpoints
 	 * @return boolean true if healthpoints > 0 | false otherwise
 	 */
